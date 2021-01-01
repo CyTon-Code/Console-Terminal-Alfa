31d04092ca8180e035877e66e9715f5249e7e6c6
CyTon Program Languages <cytoncode@gmail.com>
2021-01-02 01:41:12 +0200

    Hi I'm a module template.
    Файл содержит пример написания модуля.

--- read/libs/pattern.py
+++ read/libs/pattern.py
@@ -1,22 +1,33 @@
-def main_me(args):
+# Модуль запускается только импортируясь.
+if __name__ == "__main__": # выход если не внутри программы.
+    exit()
+
+
+# Пустая основная функция:
+def main_m(args): # Запускаемая часть модуля:
   # your code goes here
   pass
 
 
-def error_me(args):
+# Пустая аварийная функция:
+def error_m(args): # если main_m дала сбой, запускаем ее:
   # your code goes here
   pass
 
-def info_me():
+
+ # Функция для вывода слов-команд которые ожидаються в args:
+def info_m(): # функция также может выводить описание этих команд.
   print("""command:
   -my_command_1   -my_command_2""")
   pass
 
-def help_me():
+
+def help_m():
   print("""Hi I'm a module template.
 I'm here to show you what the module should look like.
 Creator Timofey Boskor""")
   pass
+
 
 """
 текущее время: 02.01.21 01:21
