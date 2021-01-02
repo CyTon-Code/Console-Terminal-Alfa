f915147e9aabbcb16729b9e59ab8898942062917
CyTon Program Languages <cytoncode@gmail.com>
2021-01-02 02:14:58 +0200

    Update main.py
    Добавлены:
    .функция проверки на возможность читать файл или его отсутствия.
    .Проверка на запуск не импортируясь.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+# Код программы, а не модуля:
+if __name__ != "__main__": # выход если внутри.
+    exit()
 
+
+# Проверить есть ли файл:
+def check_file(address_file = "libs/modul.txt"):
+    try:
+        f = open(address_file, 'r') # если ошибка, то: файла нет или
+        # его нельзя читать.
+        return True # ответ: файл есть.
+    except:
+        print("Module not defined. modul:", address_file)
+        return False # ответ: файла нет.
+    finally:
+        try:
+            f.close() # обьязательно нужно закрыть файл, если не
+            # закрыли.
+        except:
+            pass
