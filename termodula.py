b74c34841f6f576a28d5e453f9cf87a4cd56aa49
CyTon Program Languages <cytoncode@gmail.com>
2021-01-28 01:45:05 +0200

    Update termodula.py

--- termodula.py
+++ termodula.py
@@ -1,3 +1,50 @@
+import sys
+Name = "root"
+Vmail = "localhost"
+Address = ""
+
+def SortArguments(args):
+    # your code goes here
+    pass
+
+
+def GetArguments(Name = "root", Domen = "localhost", PsevdoAddress = "~"):
+    PsevdoAddress = Address
+    if Address == "":
+        PsevdoAddress="~"
+    Mail = "{}@{}".format(Name, Domen)
+    
+    print("[{} {}]".format(Mail, PsevdoAddress))
+    # your code goes here
+    pass
+
+
+def TerminalArguments():
+    # your code goes here
+    pass
+
+
+def Terminal():
+    # your code goes here
+    pass
+
+
+def TerminalFiles():
+    # your code goes here
+    pass
+
+
+def main(args):
+    args = SortArguments(args)
+    TerminalArguments(args)
+    TerminalFiles("read/term/set.term")
+    while True:
+        args = GetArguments()
+        Terminal(args)
+    # your code goes here
+    pass
+
+
 # Код программы, а не модуля:
-if __name__ != "__main__": # выход если внутри.
-    exit()
+if __name__ == "__main__": # выход если внутри.
+    main(sys.args)
